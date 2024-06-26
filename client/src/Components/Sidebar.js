46f82b3c9d590b2e111483ff11b5befd47e4de66
pikes <neutrone04@gmail.com>
2024-06-27 00:37:02 +0500

    navbar changes

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -2,12 +2,11 @@ import React, { useState, useContext } from "react";
 import "./Styles/sidebar.css";
 import axios from "axios";
 import { SidebarData } from "./SidebarData";
-import { NavLink } from "react-router-dom";
-import logo from "../Assets/Images/logo.png";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { AuthLoginInfo } from "./../AuthComponents/AuthLogin";
-import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
-import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
-import $ from "jquery"
+import HomeIcon from "@mui/icons-material/Home";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const logout = () => {
   axios
@@ -22,46 +21,32 @@ const logout = () => {
 };
 
 const NavbarSection = ({ ctx, toggleSidebar }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isDashboard = location.pathname === "/";
 
   return (
     <div>
       {ctx && (
         <nav className="mnb navbar navbar-default navbar-fixed-top">
-
           <div className="container-fluid">
+            
             <div className="navbar-header">
-              <button
-                type="button"
-                className="navbar-toggle collapsed"
-                data-toggle="collapse"
-                data-target="#navbar"
-                aria-expanded="true"
-                aria-controls="navbar"
-              // onClick={toggleSidebar}
-              >
-                <span className="sr-only">Toggle navigation</span>
-                <i className="ic fa fa-bars"></i>
-              </button>
-              <div style={{padding:'15px 0'}}>
-                <a href="#" id="msbo" onClick={toggleSidebar}>
-                  <i className="ic fa fa-bars"></i>
-                </a>
+              
+              <div className="nav-icons">
+              <div>
+              Doings ERP
+
+              </div>
+
+                <MenuIcon className="menu" onClick={toggleSidebar} />
+                <HomeIcon onClick={() => navigate('/')} style={{ cursor: 'pointer' }} />
+                {!isDashboard && <ArrowBackIcon onClick={() => navigate(-1)} style={{ cursor: 'pointer' }} />}
               </div>
             </div>
-            
-            {/* <a class="navbar-brand m-l-auto" href="#">
-              <image src="https://doingerp.com/wp-content/uploads/2023/11/New-Project-1-1.png" width="30" height="30" class="d-inline-block align-top" alt=""></image>
-            
-            </a> */}
-            <div id="navbar" className="navbar-collapse collapse">
-              <ul className="nav navbar-nav navbar-right">
-                <li className="nav-item">
-                  <a><i className="fa fa-user"></i> Logged as: {ctx.username}</a>
-                </li>
-                <li className="nav-item" onClick={logout}>
-                  <a><i className="fa fa-logout"></i>Logout</a>
-                </li>
-              </ul>
+            <div id="navbar" className="navbar-collapse collapse nav-extras">
+          
             </div>
           </div>
         </nav>
@@ -70,44 +55,48 @@ const NavbarSection = ({ ctx, toggleSidebar }) => {
   );
 };
 
-const SidebarSection = ({ ctx, sidebarClass }) => {
+const SidebarSection = ({ ctx, sidebarClass, closeSidebar }) => {
   return (
     <div>
-      
-    {ctx && (
-    <div className={sidebarClass} id="msb">
-      <nav className="navbar navbar-default" role="navigation">
-        <div className="navbar-header">
-          <div className="brand-name-wrapper">
-            <a class="navbar-brand" href="#">
-              Doing ERP
-            </a>
-          </div>
-        </div>
-        <div className="side-menu-container">
-          <ul className="nav navbar-nav">
-            {SidebarData.map((val, key) => {
-              if (val?.role !== undefined && val?.role !== ctx?.Role_Id) {
-                return null;
-              }
-              return (
-                <li key={key}>
-                  <NavLink
-                    to={val.link}
-                    className={({ isActive }) =>
-                      isActive ? "sidebar-active-link" : "sidebar-link"
-                    }
-                  >
-                    <i className={`fa ${val.icon.toLowerCase()}`}></i>
-                    {val.title}
-                  </NavLink>
+      {ctx && (
+        <div className={sidebarClass} id="msb">
+          <nav className="navbar navbar-default" role="navigation">
+            <div className="sidebar-header">
+              <ul className="nav navbar-nav navbar-right">
+                <li className="nav-item">
+                  <span><i className="fa fa-user"></i> Logged as: {ctx.username}</span>
                 </li>
-              );
-            })}
-          </ul>
+                <li className="nav-item logout" onClick={logout}>
+                  <span><i className="fa fa-logout"></i>Logout</span>
+                </li>
+              </ul>
+            </div>
+            <div className="side-menu-container">
+              <ul className="nav navbar-nav">
+                {SidebarData.map((val, key) => {
+                  if (val?.role !== undefined && val?.role !== ctx?.Role_Id) {
+                    return null;
+                  }
+                  return (
+                    <li key={key}>
+                      <NavLink
+                        to={val.link}
+                        className={({ isActive }) =>
+                          isActive ? "sidebar-active-link" : "sidebar-link"
+                        }
+                        onClick={closeSidebar}
+                      >
+                        <i className={`fa ${val.icon.toLowerCase()}`}></i>
+                        {val.title}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </nav>
         </div>
-      </nav>
-    </div>)}
+      )}
     </div>
   );
 };
@@ -117,20 +106,21 @@ function Sidebar() {
   const [sidebarClass, setSidebarClass] = useState("msb");
 
   const toggleSidebar = () => {
-    (function () {
-      $('#msbo').on('click', function () {
-        $('body').toggleClass('msb-x');
-      });
-    }());
+    setSidebarClass(sidebarClass === "msb" ? "msb msb-x" : "msb");
+    document.body.classList.toggle('msb-x');
+  };
+
+  const closeSidebar = () => {
+    setSidebarClass("msb msb-x");
+    document.body.classList.add('msb-x');
   };
 
   return (
     <div className="SidebarWrapper">
       <NavbarSection ctx={ctx} toggleSidebar={toggleSidebar} />
-      <SidebarSection ctx={ctx} sidebarClass={sidebarClass} />
+      <SidebarSection ctx={ctx} sidebarClass={sidebarClass} closeSidebar={closeSidebar} />
     </div>
   );
 }
 
 export default Sidebar;
-
